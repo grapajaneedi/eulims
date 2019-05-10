d326bb11b7915c4f5f3cc4d013d5c2962994d131
grapajaneedi <grapajaneedi@gmail.com>
2019-05-10 15:19:43 +0800

    db config

--- common/config/db.php
+++ common/config/db.php
@@ -1,50 +1,55 @@
 <?php
+
+$server = "192.168.1.96";
+$username = "eulims";
+$password = "D05793ul1ms!@#$%";
+
 return [
     'db'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=localhost;dbname=eulims',
-        'username' => 'eulims',
-        'password' => 'eulims',
+        'dsn' => 'mysql:host='.$server.';dbname=eulims',
+        'username' => $username,
+        'password' => $password,
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
     'labdb'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=localhost;dbname=eulims_lab',
-        'username' => 'eulims',
-        'password' => 'eulims',
+       'dsn' => 'mysql:host='.$server.';dbname=eulims_lab',
+        'username' => $username,
+        'password' => $password,
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
     'inventorydb'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=localhost;dbname=eulims_inventory',
-        'username' => 'eulims',
-        'password' => 'eulims',
+        'dsn' => 'mysql:host='.$server.';dbname=eulims_inventory',
+        'username' => $username,
+        'password' => $password,
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
     'financedb'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=localhost;dbname=eulims_finance',
-        'username' => 'eulims',
-        'password' => 'eulims',
+        'dsn' => 'mysql:host='.$server.';dbname=eulims_finance',
+        'username' => $username,
+        'password' => $password,
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
     'addressdb'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=localhost;dbname=eulims_address',
-        'username' => 'eulims',
-        'password' => 'eulims',
+        'dsn' => 'mysql:host='.$server.';dbname=eulims_address',
+        'username' => $username,
+        'password' => $password,
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
     'referraldb'=>[
         'class' => 'yii\db\Connection',  
-        'dsn' => 'mysql:host=localhost;dbname=eulims_referral_lab',
-        'username' => 'eulims',
-        'password' => 'eulims',
+        'dsn' => 'mysql:host='.$server.';dbname=eulims_referral_lab',
+        'username' => $username,
+        'password' => $password,
         'charset' => 'utf8',
         'tablePrefix' => 'tbl_',
     ],
