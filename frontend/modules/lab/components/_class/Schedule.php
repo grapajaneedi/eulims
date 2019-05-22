e54496a6cf877fe9e3cde55e0fe140f1e554dbba
Bergel <b.cutara@gmail.com>
2019-05-22 14:52:03 +0800

    test schedules

---
+++ frontend/modules/lab/components/_class/Schedule.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace frontend\modules\lab\components\_class;
+/**
+ * 
+ */
+class Schedule
+{
+	public $id;
+	public $title;
+	public $start;
+	public $end;
+	public $textColor;
+	public $backgroundColor;
+}
+?>
