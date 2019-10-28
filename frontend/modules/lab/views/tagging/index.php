7cbd0f255a0e4b02c2126a5cf660096184f3f3d8
grapajaneedi <grapajaneedi@gmail.com>
2019-10-28 13:34:34 +0800

    updates

--- frontend/modules/lab/views/tagging/index.php
+++ frontend/modules/lab/views/tagging/index.php
@@ -23,41 +23,7 @@ use kartik\widgets\Select2;
 /* @var $searchModel common\models\TaggingSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$lablist= ArrayHelper::map(Sample::find()->all(),'sample_id','sample_code');
-$sampletypelist= ArrayHelper::map(Sampletype::find()->all(),'sampletype_id','type');
-
-$this->title = 'Tagging';
-$this->params['breadcrumbs'][] = ['label' => 'Tagging', 'url' => ['/lab']];
-$this->params['breadcrumbs'][] = 'Sample Tagging';
-
-$this->registerJsFile("/js/services/services.js");
-$func=new Functions();
-
-
 ?>
-<?php ini_set("memory_limit", "100000M");?>
-<!-- <blockquote class="imgur-embed-pub" lang="en" data-id="a/lc3D4"><a href="//imgur.com/lc3D4">Tadpole Lessons</a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script> -->
-
-<div class="tagging-index">
-    <?php
-        echo $func->GenerateStatusTagging("Legend/Status",true);
-    ?>
-
-<div class="alert alert-info" style="background: #d4f7e8 !important;margin-top: 1px !important;">
-     <a href="#" class="close" data-dismiss="alert" >×</a>
-    <p class="note" style="color:#265e8d"><b>Note:</b> Please scan barcode in the dropdown list below. .</p>
-     
-    </div>
-    <div class="row">
-    <div class="col-md-4">
-       <?php $form = ActiveForm::begin(); ?>
-       <?php
-               $disabled=false; 
-               echo $func->GetSampleCode($form,$model,$disabled,"");
-       ?>    
-       <?php ActiveForm::end(); ?>
-           </div>
-              </div>
 
  <div class="row-fluid" id ="xyz">
     <?= GridView::widget([
--- frontend/views/admin-lte/layouts/left.php
+++ frontend/views/admin-lte/layouts/left.php
@@ -120,9 +120,9 @@ if(Yii::$app->user->isGuest){
                 $pkgdetails2=str_replace(" ","-",$pkgdetails1);
                 $SubmodulePermission="access-".$pkgdetails2; //access-Order of Payment
 				if($mItem->extra_element == 1){
-					$numNotification = '&nbsp;&nbsp;<span class="label label-danger" id="count_noti_sub_referral">'.$unresponded.'</span>';
-					$showURL = '#';
-					$template = '<a href="{url}" onclick="showNotifications()" id="btn_unresponded">{label}</a>';
+					// $numNotification = '&nbsp;&nbsp;<span class="label label-danger" id="count_noti_sub_referral">'.$unresponded.'</span>';
+					// $showURL = '#';
+					// $template = '<a href="{url}" onclick="showNotifications()" id="btn_unresponded">{label}</a>';
 				} elseif ($mItem->extra_element == 2) {
 					$numNotification = '&nbsp;&nbsp;<span class="label label-danger" id="count_noti_sub_bid">'.$unseen.'</span>';
 					$showURL = '#';
@@ -153,16 +153,16 @@ if(Yii::$app->user->isGuest){
             // }
 			
             $MainIcon=substr($Item->icon,6,strlen($Item->icon)-6);
-			$showNotification = (stristr($Item->PackageName, 'referral')) ? '&nbsp;&nbsp;<span class="label label-danger" id="count_noti_menu">'.$all_notification.'</span>' : '';
-            $ItemMenu[]=[
-                'label' => '<img src="/images/icons/' .$Item->icon. '.png" style="width:20px">  <span>' . ucwords($Item->PackageName) . $showNotification . '</span>', 
-                'icon'=>' " style="display:none;width:0px"',
-                'url' => "",
-                //'url' => ["/".$Item->PackageName."/index"],
-                'items'=>$ItemSubMenu,
-                'visible'=>Yii::$app->user->can($modulePermission)
-            ]; 
-            unset($ItemSubMenu);
+			//$showNotification = (stristr($Item->PackageName, 'referral')) ? '&nbsp;&nbsp;<span class="label label-danger" id="count_noti_menu">'.$all_notification.'</span>' : '';
+            // $ItemMenu[]=[
+            //     'label' => '<img src="/images/icons/' .$Item->icon. '.png" style="width:20px">  <span>' . ucwords($Item->PackageName) . $showNotification . '</span>', 
+            //     'icon'=>' " style="display:none;width:0px"',
+            //     'url' => "",
+            //     //'url' => ["/".$Item->PackageName."/index"],
+            //     'items'=>$ItemSubMenu,
+            //     'visible'=>Yii::$app->user->can($modulePermission)
+            // ]; 
+             unset($ItemSubMenu);
         }
         // Fixed Sub Menu Item
         $SubItem=[
@@ -197,15 +197,16 @@ if(Yii::$app->user->isGuest){
             ],
             'visible'=>Yii::$app->user->can('access-system')
         ];
-        array_push($ItemMenu, $SubItem);
+        //array_push($ItemMenu, $SubItem);
         ?>
-         <?php echo dmstr\widgets\Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
-                'items' => $ItemMenu,
-                'encodeLabels' => false,
-            ]
-        );
+         <?php 
+        //  echo dmstr\widgets\Menu::widget(
+        //     [
+        //         'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
+        //         'items' => $ItemMenu,
+        //         'encodeLabels' => false,
+        //     ]
+        // );
         ?>
     </section>
 
