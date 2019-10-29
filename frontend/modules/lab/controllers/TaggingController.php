f52f5b658c1e1b6d4ad67df168fd7e7facdc809d
grapajaneedi <grapajaneedi@gmail.com>
2019-10-29 11:19:29 +0800

    test

--- frontend/modules/lab/controllers/TaggingController.php
+++ frontend/modules/lab/controllers/TaggingController.php
@@ -18,7 +18,7 @@ use yii\filters\VerbFilter;
 use yii\db\Query;
 use yii\data\ActiveDataProvider;
 use common\models\system\Profile;
-ini_set("memory_limit", "200M");
+//ini_set("memory_limit", "200M");dfgf
 /**
  * TaggingController implements the CRUD actions for Tagging model.
  */
